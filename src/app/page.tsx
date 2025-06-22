51b3f97e1aca6acbf5f0f330b39717399bfd46ff
7heProtagonist <30697367+7h3Pr074g0n157@users.noreply.github.com>
2025-06-22 10:14:31 +0200

    Implement registerHandler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 import Image from 'next/image';
+import { registerHandler } from './db';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <form action="" className="border-2 border-black rounded p-4">
+        <form
+          action={registerHandler}
+          className="border-2 border-black rounded p-4">
           <div className="p-4">
             <label htmlFor="username">Username</label>
-            <input type="text" className="border-2" />
+            <input type="text" className="border-2" name="username" />
           </div>
           <div className="p-4">
             <label htmlFor="password">Password dummy</label>
-            <input type="text" className="border-2" />
+            <input type="text" className="border-2" name="email" />
           </div>
           <button
             type="submit"
