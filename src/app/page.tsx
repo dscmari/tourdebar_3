652a9ef82e03d2fb54dc68c3fccd6d7666e1c7f2
Marian Noekel <marian@MacCarlo.fritz.box>
2025-07-03 22:42:50 +0200

    delete prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image';
-import { registerHandler } from './db';
+// import { registerHandler } from './db';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <form
-          action={registerHandler}
+          // action={registerHandler}
           className="border-2 border-black rounded p-4">
           <div className="p-4">
             <label htmlFor="username">Username</label>
