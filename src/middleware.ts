5a4607ff05310918c9606ea7385037ee6c3301d1
Marian Noekel <marian@MacCarlo.fritz.box>
2025-08-19 10:27:04 +0200

    instead adapting tailwind config use globals.css , clean /register

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,5 +22,5 @@ export default async function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|login|register).*)']
+  matcher: ['/((?!_next/static|_next/image|api|favicon.ico|.*\\.png$|.*\\.jpg$).*)']
 };
